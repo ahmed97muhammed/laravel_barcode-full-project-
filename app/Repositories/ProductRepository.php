7b9247f1f98d85a12849d1975701ad68a3d50fd8
Ahmed Muhammed <86477530+ahmed97muhammed@users.noreply.github.com>
2021-10-09 04:22:19 +0200

    Update ProductRepository.php

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -4,75 +4,42 @@ use App\Product;
 use Validator;
 class ProductRepository implements ProductRepositoryInterface
 {
-public function getAll(){
-return Product::all();
-}
+    public function getAll()
+    {
+        return Product::all();
+    }
 
-public function getProduct($id){
-return Product::findOrFail($id);
-}
+    public function getProduct($id)
+    {
+        return Product::findOrFail($id);
+    }
 
-////////////////////////Start Create Product ///////////////////////////
+    public function createProduct($product)
+    {
+        $validation = Validator::make($product, [
+        'product_name' => ['required'],
+        'purchasing_price'=>['required','numeric'],
+        'selling_price'=>['required','numeric'],
 
-public function createProduct($product)
-{
-	
-$validation = Validator::make($product, [
-'product_name' => ['required'],
-'purchasing_price'=>['required','numeric'],
-'selling_price'=>['required','numeric'],
+        ]);
+        return $validation->passes()?Product::create($product):response()->json('error',500);
+    }
+    
+    public function updateProduct($product,$id)
+    {
+        $validation = Validator::make($product, [
+        'product_name' => ['required'],
+        'purchasing_price'=>['required','numeric'],
+        'selling_price'=>['required','numeric'],
+        ]);
+        return $validation->passes()?Product::findOrFail($id)->update($product):response()->json('error',500);
+    }
 
-]);
-
-if($validation->passes())
-{
-
-	return Product::create($product);
-
-}
-else{
-	return response()->json('error',500);
-}
-
-}
-////////////////////////End Create Product //////////////////////////////////
-
-////////////////////////Start Update Product ////////////////////////////////
-
-public function updateProduct($product,$id)
-{
-
-$validation = Validator::make($product, [
-'product_name' => ['required'],
-'purchasing_price'=>['required','numeric'],
-'selling_price'=>['required','numeric'],
-
-]);
-
-if($validation->passes())
-{
-
-if(Product::findOrFail($id))
-{
-return Product::whereId($id)->update($product);
-}
-
-}
-
-else{
-return response()->json('error',500);
-}
-
-}
-
-////////////////////////End Update Product ////////////////////////////////
-
-////////////////////////Start Get Count of Products //////////////////////
-public function getcountproducts()
-{
-return Product::count();
-}
-////////////////////////End Get Count of Products //////////////////////
+    public function getcountproducts()
+    {
+        return Product::count();
+    }
+    
 
 
 }
